0255d8c1c02c15357324af26e249805b16632209
Josh Grossman <tghosth@users.noreply.github.com>
2023-06-27 16:11:57 +0300

    Make test case clearer

--- code/python/dosomething.py
+++ code/python/dosomething.py
@@ -15,6 +15,14 @@ def bad3(): #semgrep supressed
     cipher = AES.new("", AES.MODE_CFB, iv)
     msg = iv + cipher.encrypt(b'Attack at dawn')
 
+def bad4(): #semgrep supressed
+    cipher = AES.new("", AES.MODE_CFB, iv) # nosemgrep
+    msg = iv + cipher.encrypt(b'Attack at dawn')
+
+def bad3(): #semgrep supressed
+    cipher = AES.new("", AES.MODE_CFB, iv) # nosemgrep: empty-aes-key
+    msg = iv + cipher.encrypt(b'Attack at dawn')
+
 def ok1(key):
     # ok: empty-aes-key
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
